d11fe5f60bf626825640da3fbf7ceea761e978b3
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-02 19:29:13 +0200

    Create a JWT Token Generation/Login Rest API in Spring Boot

--- src/main/java/com/carrenta/controller/AuthController.java
+++ src/main/java/com/carrenta/controller/AuthController.java
@@ -1,21 +1,42 @@
 package com.carrenta.controller;
 
+import com.carrenta.dto.AuthenticationRequest;
+import com.carrenta.dto.AuthenticationResponse;
 import com.carrenta.dto.SignupRequest;
 import com.carrenta.dto.UserDto;
+import com.carrenta.entity.User;
+import com.carrenta.repository.UserRepository;
 import com.carrenta.service.auth.AuthService;
+import com.carrenta.service.jwt.UserService;
+import com.carrenta.utils.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
     @PostMapping("/signup")
     public ResponseEntity<?> signupCustomer(@RequestBody SignupRequest signupRequest){
         if (authService.hasCustomerWithEmail(signupRequest.getEmail())) {
@@ -27,5 +48,28 @@ public class AuthController {
             return new ResponseEntity<>("Customer not created, try again later", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(createdCustomerDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+        throws BadCredentialsException, DisabledException, UsernameNotFoundException
+    {
+       try {
+           authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                   authenticationRequest.getEmail(),
+                   authenticationRequest.getPassword()));
+       } catch (BadCredentialsException e) {
+           throw new BadCredentialsException("Incorrect username or password");
+       }
+       final UserDetails userDetails = userService.userDetailsService().loadUserByUsername(authenticationRequest.getEmail());
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        if (optionalUser.isPresent()) {
+            authenticationResponse.setJwt(jwt);
+            authenticationResponse.setUserId(optionalUser.get().getId());
+            authenticationResponse.setUserRole(optionalUser.get().getUserRole());
+        }
+        return authenticationResponse;
     }
 }
