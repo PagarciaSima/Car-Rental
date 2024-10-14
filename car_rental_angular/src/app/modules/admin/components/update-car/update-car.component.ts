af51c5a370f924a35aa21963fe474a1d5734e4a3
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-14 07:05:13 +0200

    Displaying Car Details in Update Car Reactive Form in Angular

--- car_rental_angular/src/app/modules/admin/components/update-car/update-car.component.ts
+++ car_rental_angular/src/app/modules/admin/components/update-car/update-car.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AdminService } from '../../service/admin.service';
 import { ActivatedRoute } from '@angular/router';
+import { AdminService } from '../../service/admin.service';
+import { CarDto } from 'src/app/models/car.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-car',
@@ -10,15 +12,35 @@ import { Subscription } from 'rxjs';
 })
 export class UpdateCarComponent implements OnInit, OnDestroy {
 
+  isSpinning = false;
   carId: number = 0;
-  private paramsSubscription: Subscription = new Subscription();
+  existingImage: string | null = null;
+  paramsSubscription: Subscription = new Subscription();
+  updateForm!: FormGroup;
+  listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"];
+  listOfOption: Array<{label:string, value:string}> = [];
+  listOfType = ["Petrol", "Hybrid", "Diesel", "Electric", "CNG"];
+  listOfColor = ["Red", "White", "Blue", "Black", "Orange", "Grey", "Silver"];
+  listOfTransmission = ["Manual", "Automatic"];
 
   constructor(
     private adminService: AdminService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.updateForm = this.fb.group ({
+      name: [null, Validators.required],
+      brand: [null, Validators.required],
+      type: [null, Validators.required],
+      color: [null, Validators.required],
+      transmission: [null, Validators.required],
+      price: [null, Validators.required],
+      description: [null, Validators.required],
+      year: [null, Validators.required]
+    });
+
     this.paramsSubscription = this.activatedRoute.params.subscribe(params => {
       this.carId = +params['id'];
     });
@@ -32,9 +54,14 @@ export class UpdateCarComponent implements OnInit, OnDestroy {
   }
 
   getByCarById() {
+    this.isSpinning = true;
     this.adminService.getCarById(this.carId).subscribe({
       next: (res) => {
-        console.log(res);
+        this.isSpinning = false;
+        const carDto = JSON.stringify(res, null, 2);
+        this.existingImage = 'data:image/jpeg;base64,' + res.returnedImage;
+        console.log("carDTO " + carDto);
+        this.updateForm.patchValue(res);
       },
       error: (error) => {
         console.error('Error al obtener el coche:', error);
